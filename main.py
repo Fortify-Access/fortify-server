31da116934e6753b1949e11310ba1a25b2cbfd5f
Fortify Access <fortifyaccess@gmail.com>
2023-07-30 00:07:58 +0330

    Main file to run project added.

---
+++ main.py
@@ -0,0 +1,37 @@
+import contextlib
+import asyncio
+from starlette.applications import Starlette
+from starlette.routing import Route
+from api.middleware import TokenAuthMiddleware
+from api import endpoints
+from api import tasks
+
+@contextlib.asynccontextmanager
+async def lifespan(app):
+    asyncio.create_task(tasks.check_expirations(300))
+    asyncio.create_task(tasks.check_traffic_usages())
+    asyncio.create_task(tasks.commit_traffic_usages_to_db(5))
+    yield
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route("/inbound/list", endpoint=endpoints.inbound_list, methods=["GET"]),
+        Route("/inbound/get", endpoint=endpoints.inbound_get, methods=["GET"]),
+        Route("/inbound/create", endpoint=endpoints.inbound_create, methods=["POST"]),
+        Route("/inbound/update", endpoint=endpoints.inbound_create, methods=["PUT"]),
+        Route("/inbound/delete", endpoint=endpoints.inbound_delete, methods=["DELETE"]),
+    ],
+    middleware=[(TokenAuthMiddleware, {})],
+    lifespan=lifespan
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    from sqlmodel import SQLModel
+    from api import models
+    from api.extentions import engine
+
+    SQLModel.metadata.create_all(engine)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
