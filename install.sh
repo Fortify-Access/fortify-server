f3f8a91750e93f6d5747a05702ad216fd90b5826
Fortify Access <fortifyaccess@gmail.com>
2023-07-30 17:13:42 +0330

    Asking user for entering api port.

--- install.sh
+++ install.sh
@@ -69,15 +69,21 @@ install_project() {
   source venv/bin/activate
   pip install -r requirements.txt
 
+  read -p "Enter a port number for API calling (default is 8000): " api_port
+  default_port=8000
+  port=${api_port:-$default_port}
+
   auth_key=$(generate_token)
-  echo -e "AUTH_KEY=$auth_key\nFORTIFY_SERVER_VERSION=1.0.0\nSINGBOX_VERSION=1.3.0" > .env
+  echo -e "AUTH_KEY=$auth_key\nAPI_PORT=$port\nFORTIFY_SERVER_VERSION=1.0.0\nSINGBOX_VERSION=1.3.0" > .env
 
   # Step 4: Downlaod and extract sing-box binary
   echo "Step 4: Downloading sing-box..."
   if [[ "$(uname -m)" == "x86_64" ]]; then
       package_url="https://github.com/SagerNet/sing-box/releases/download/v1.3.0/sing-box-1.3.0-linux-amd64.tar.gz"
+      package_name="sing-box-1.3.0-linux-amd64"
   elif [[ "$(uname -m)" == "aarch64" ]]; then
       package_url="https://github.com/SagerNet/sing-box/releases/download/v1.3.0/sing-box-1.3.0-linux-arm64.tar.gz"
+      package_name="sing-box-1.3.0-linux-arm64"
   else
       echo "Unsupported system architecture."
       exit 1
@@ -97,7 +103,6 @@ install_project() {
 
   # Step 7: Prepare package names
   echo "Step 7: Prepare package names..."
-  package_name="singbox"
   # Download the latest release package (.tar.gz) from GitHub
   curl -sLo "/opt/fortify-server/${package_name}.tar.gz" $package_url
   # Extract the package and move the binary to /root
@@ -112,4 +117,4 @@ install_project() {
 install_docker_and_redis
 install_project
 echo "Installation completed successfully."
-echo -e "IMPORTANT:\nHere is your generated auth key to access this server:\n$auth_key\nCopy this key and paste it to your Fortify Assess panel."
+echo -e "IMPORTANT:\nHere is your generated auth key to access this server:\n$auth_key\nCopy above key and paste it to your Fortify Assess panel.\nAlso the API port is: $port"
